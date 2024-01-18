e53ad8cc7976783d4be6e8d5cc1d1c93628385fe
Jesus martin romero <jesusmartinromero10@gmail.com>
2024-01-18 10:17:20 +0100

    prueba6

--- test_division.py
+++ test_division.py
@@ -9,7 +9,11 @@ class TestDivision(unittest.TestCase):
         self.assertEqual(dividir(-8, 2), -4)
         self.assertEqual(dividir(8, -2), -4)
         self.assertEqual(dividir(-8, -2), 4)
-        self.assertEqual(dividir(8, 0), ZeroDivisionError)
+        
+        
+    def test_division_por_cero(self):
+        with self.assertRaises(ZeroDivisionError):
+            resultado = dividir(10, 0)   
 
 if __name__ == "__main__":
     unittest.main()
