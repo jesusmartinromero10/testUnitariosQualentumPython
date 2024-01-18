493e389ef37352cc6ba1dc4af0ce708bf2352681
Jesus martin romero <jesusmartinromero10@gmail.com>
2024-01-18 11:40:22 +0100

    prueba final2

--- test_suma.py
+++ test_suma.py
@@ -5,6 +5,6 @@ class TestSumar(unittest.TestCase):
     self.assertEqual(sumar(3, 2), 5)
     self.assertEqual(sumar(-1, 1), 0)
     self.assertEqual(sumar(-1, -1), -2)
-    self.assertEqual(sumar(1, 1), 2)
+    
 if __name__ == '__main__':
  unittest.main()
